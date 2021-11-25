230eea1bc91dc3069ed936d94b7a43a94778841d
Franka Keil <fkeil@lcs.on.ca>
2021-11-25 09:17:06 -0500

    We abstracted out the data from the view. Instead we used a ForEach to build the list of names

--- ReportAbstractionExample/Model/Report.swift
+++ ReportAbstractionExample/Model/Report.swift
@@ -7,14 +7,25 @@
 
 import Foundation
 
-struct Report {
+// MODEL
+// Simply a discription of the data that our app needs to show
+// "Report" conforms to the Identifiable protocol... this just means that we promise to include an "id" property that will contain a unique identifier
+struct Report: Identifiable {
     
+    // MARK: Stored properties
+    // This property is automatically given a value
+    // The = is an assignment, it is gettting an INSTANCE of the UUID structure
+    let id = UUID()
+    
+    // These properties must be initialized with an argument
     let name: String
     let grade: Int
     let comment: String
     
 }
 
+// Storing the data that won't change - the data we want to display
+// A list of INSTANCES of the Report structure
 let listOfReports = [
     Report(name: "Bethany Barr",
            grade: 50,
